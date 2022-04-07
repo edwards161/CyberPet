83533a84de1b1230780c747638b4181680237152
edwards161 <acejakey@gmail.com>
2022-04-07 13:52:06 +0100

    added event listeners

--- main.js
+++ main.js
@@ -5,6 +5,13 @@ const catImage = document.getElementById("catImage");
 choosePet.style.display = "flex";
 petUI.style.display = "none";
 
+class Animal {
+    constructor(name){
+        this._name = name;
+        this._hunger = 50;
+        this._happiness = 50;
+    }
+}
 
 dogImage.addEventListener("click", () => {
     choosePet.style.display = "none";
@@ -17,3 +24,15 @@ catImage.addEventListener("click", () => {
     petUI.style.display = "flex";
     petImg.src = "images/Tom_Cat.webp";
 })
+
+mouseImage.addEventListener("click", () => {
+    choosePet.style.display = "none";
+    petUI.style.display = "flex";
+    petImg.src = "images/Jerry_Mouse.png";
+})
+
+newPet.addEventListener("click", () => {
+    choosePet.style.display ="flex";
+    petUI.style.display = "none";
+})
+
